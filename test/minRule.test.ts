c28b8bcbcd65071b3e4c6cd964b0defcbbe8df6d
sot1986 <matteo.presot@gmail.com>
2025-12-28 11:44:52 +0100

    change parse method name

--- test/minRule.test.ts
+++ test/minRule.test.ts
@@ -7,7 +7,7 @@ describe('test minRule validation', () => {
     'a'.repeat(10),
     'a'.repeat(500),
   ])('validates min lenght string', (value) => {
-    const result = rules.validate(value, 'min:2')
+    const result = rules.parse(value, 'min:2')
     expect(result.check).toBe(true)
   })
 })
